52c8d2e37104090e169cf336a2e7a820c7cc372e
itfadhila <pujamalca09@gmail.com>
2025-01-21 14:32:32 +0700

    tambahkan migrasi id ke tabel tracker dan trackersql

---
+++ database/migrations/2025_01_21_072748_add_id_to_trackersql_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trackersql', function (Blueprint $table) {
+            // Tambahkan kolom id hanya jika tabel belum memiliki primary key lain
+            if (!Schema::hasColumn('trackersql', 'id')) {
+                $table->id()->first(); // Menambahkan kolom id di awal tabel
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trackersql', function (Blueprint $table) {
+            if (Schema::hasColumn('trackersql', 'id')) {
+                $table->dropColumn('id'); // Hapus kolom id jika rollback
+            }
+        });
+    }
+};
